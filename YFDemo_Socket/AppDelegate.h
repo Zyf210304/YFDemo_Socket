488bda1d960cf222fb493b1c4c5d37d3d54acd5e
Zyf210304 <672233580@qq.com>
2018-04-13 09:09:50 +0800

    Initial Commit

---
+++ YFDemo_Socket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YFDemo_Socket
+//
+//  Created by 亚飞 on 2018/4/13.
+//  Copyright © 2018年 yafei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
